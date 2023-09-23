767c8a80fd2af57ff1e6acae678907596380b630
Moji00f <mojtabaahmadi2008@gmail.com>
2023-09-23 11:42:57 +0330

    fix bug of channel/connection

--- listener-service/event/consumer.go
+++ listener-service/event/consumer.go
@@ -1,3 +1,133 @@
+// package event
+
+// import (
+// 	"bytes"
+// 	"encoding/json"
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+
+// 	amqp "github.com/rabbitmq/amqp091-go"
+// )
+
+// type Consumer struct {
+// 	conn      *amqp.Connection
+// 	queueName string
+// }
+
+// func NewConsumer(conn *amqp.Connection) (Consumer, error) {
+// 	consumer := Consumer{
+// 		conn: conn,
+// 	}
+
+// 	err := consumer.setup()
+// 	if err != nil {
+// 		return Consumer{}, err
+// 	}
+
+// 	return consumer, nil
+// }
+
+// func (consumer *Consumer) setup() error {
+// 	channel, err := consumer.conn.Channel()
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	return declareExchange(channel)
+// }
+
+// type Payload struct {
+// 	Name string `json:"name"`
+// 	Data string `json:"data"`
+// }
+
+// func (consumer *Consumer) Listen(topics []string) error {
+// 	ch, err := consumer.conn.Channel()
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	defer ch.Close()
+
+// 	q, err := declareRandomQueue(ch)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	for _, s := range topics {
+// 		ch.QueueBind(q.Name, s, "logs_topic", false, nil)
+// 	}
+
+// 	messages, err := ch.Consume(q.Name, "", true, false, false, false, nil)
+// 	if err != nil {
+// 		fmt.Println("llllllllllllllllll")
+// 		return err
+// 	}
+
+// 	forever := make(chan bool)
+// 	go func() {
+// 		for d := range messages {
+// 			var payload Payload
+// 			_ = json.Unmarshal(d.Body, &payload)
+// 			go handlePayload(payload)
+// 		}
+// 	}()
+
+// 	fmt.Printf("Waiting for message [Exchange, Qeue] [logs_topics, %s]\n", q.Name)
+// 	<-forever
+
+// 	return nil
+// }
+
+// func handlePayload(payload Payload) {
+// 	switch payload.Name {
+// 	case "log", "event":
+// 		//log whatever we get
+// 		err := logEvent(payload)
+// 		if err != nil {
+// 			log.Println(err)
+// 		}
+
+// 	case "auth":
+// 		//authenticate
+// 		// you can have as many cases as you want, as long as you write the logic
+// 	default:
+// 		err := logEvent(payload)
+// 		if err != nil {
+// 			log.Println(err)
+// 		}
+// 	}
+// }
+
+// func logEvent(entry Payload) error {
+// 	jsonData, _ := json.MarshalIndent(entry, "", "\t")
+
+// 	logServiceURL := "http://logger-service/log"
+
+// 	request, err := http.NewRequest("POST", logServiceURL, bytes.NewBuffer(jsonData))
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	request.Header.Set("Content-Type", "application/json")
+
+// 	client := &http.Client{}
+
+// 	response, err := client.Do(request)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	defer response.Body.Close()
+
+// 	if response.StatusCode != http.StatusAccepted {
+// 		return err
+// 	}
+
+// 	return nil
+// }
+
 package event
 
 import (
@@ -47,7 +177,6 @@ func (consumer *Consumer) Listen(topics []string) error {
 	if err != nil {
 		return err
 	}
-
 	defer ch.Close()
 
 	q, err := declareRandomQueue(ch)
@@ -56,12 +185,21 @@ func (consumer *Consumer) Listen(topics []string) error {
 	}
 
 	for _, s := range topics {
-		ch.QueueBind(q.Name, s, "logs_topic", false, nil)
+		ch.QueueBind(
+			q.Name,
+			s,
+			"logs_topics",
+			false,
+			nil,
+		)
+
+		if err != nil {
+			return err
+		}
 	}
 
 	messages, err := ch.Consume(q.Name, "", true, false, false, false, nil)
 	if err != nil {
-		fmt.Println("llllllllllllllllll")
 		return err
 	}
 
@@ -70,11 +208,12 @@ func (consumer *Consumer) Listen(topics []string) error {
 		for d := range messages {
 			var payload Payload
 			_ = json.Unmarshal(d.Body, &payload)
+
 			go handlePayload(payload)
 		}
 	}()
 
-	fmt.Printf("Waiting for message [Exchange, Qeue] [logs_topics, %s]\n", q.Name)
+	fmt.Printf("Waiting for message [Exchange, Queue] [logs_topic, %s]\n", q.Name)
 	<-forever
 
 	return nil
@@ -83,15 +222,17 @@ func (consumer *Consumer) Listen(topics []string) error {
 func handlePayload(payload Payload) {
 	switch payload.Name {
 	case "log", "event":
-		//log whatever we get
+		// log whatever we get
 		err := logEvent(payload)
 		if err != nil {
 			log.Println(err)
 		}
 
 	case "auth":
-		//authenticate
-		// you can have as many cases as you want, as long as you write the logic
+		// authenticate
+
+	// you can have as many cases as you want, as long as you write the logic
+
 	default:
 		err := logEvent(payload)
 		if err != nil {
--- listener-service/main.go
+++ listener-service/main.go
@@ -37,29 +37,63 @@ func main() {
 	}
 }
 
+// func connect() (*amqp.Connection, error) {
+// 	var counts int64
+// 	var backOff = 1 * time.Second
+// 	var connection *amqp.Connection
+
+// 	// don't continue until rabbitmq is ready
+// 	for {
+// 		c, err := amqp.Dial("amqp://guest:guest@localhost")
+// 		if err != nil {
+// 			fmt.Println("RabbitMQ not yet ready ...")
+// 			counts++
+// 		} else {
+// 			log.Println("Connected to RabbitMQ")
+// 			connection = c
+// 			break
+// 		}
+
+// 		if counts > 5 {
+// 			fmt.Println(err)
+// 			return nil, err
+// 		}
+
+// 		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+// 		log.Println("backing off ...")
+// 		time.Sleep(backOff)
+// 		continue
+// 	}
+
+// 	return connection, nil
+// }
+
 func connect() (*amqp.Connection, error) {
 	var counts int64
 	var backOff = 1 * time.Second
 	var connection *amqp.Connection
 
-	// don't continue until rabbitmq is ready
+	// don't continue until rabbit is ready
 	for {
-		c, err := amqp.Dial("amqp://guest:guest@localhost")
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
 		if err != nil {
-			fmt.Println("RabbitMQ not yet ready ...")
+			fmt.Println("RabbitMQ not yet ready...")
 			counts++
 		} else {
-			log.Println("Connected to RabbitMQ")
+			log.Println("Connected to RabbitMQ!")
 			connection = c
 			break
 		}
 
 		if counts > 5 {
-			backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
-			log.Println("backing off ...")
-			time.Sleep(backOff)
-			continue
+			fmt.Println(err)
+			return nil, err
 		}
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("backing off...")
+		time.Sleep(backOff)
+		continue
 	}
 
 	return connection, nil
